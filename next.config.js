7fabd2f1b8cb9b57153cda37a55b6649b3ae65f1
joy9163 <joy9163@joy9163deMacBook-Air-5.local>
2026-03-19 17:57:12 +0800

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // appDir is now the default in Next.js 13+, no need for experimental flag
+  output: 'export',
+  images: {
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig
